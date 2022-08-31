ca496bdc70fe88b1d3145049c75f5e7dc4d82220
hj-yuki <hjystfc07gd2020@gmail.com>
2022-08-31 22:10:54 +0900

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
